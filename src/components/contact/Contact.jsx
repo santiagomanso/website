bd951611a45ab85988266f7af67e6fee0e82d4dd
Santiago Manso <santiagomanso@web.de>
2022-05-29 12:54:55 +0200

    test 1.0

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
-  return <section id='contact'>Contact</section>
+  return (
+    <section id='contact'>
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>mansi.manito@gmail.com</h5>
+            <a href='mailto:mansi.manito@gmail.com' rel='norefer'>
+              Send a message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>+491744538564</h5>
+            <a
+              href='https://api.whatsapp.com/send?phone=491744538564'
+              rel='noreferrer'
+              target='_blank'
+            >
+              Whatsapp me
+            </a>
+          </article>
+        </div>
+
+        <form action=''>
+          <input type='text' placeholder='Your full name' required />
+          <input type='email' name='email' placeholder='Your email' required />
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Your message'
+            required
+          ></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,160 +3,65 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Inventory Manager',
+    github: 'https://github.com/santiagomanso/inventory',
+    demo: 'https://inventory.santimanso.com/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Heroes App',
+    github: 'https://github.com/santiagomanso/heroes-app',
+    demo: 'https://exquisite-rolypoly-147bef.netlify.app/',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Portfolio Landing Page',
+    github: 'https://github.com/santiagomanso/portfolio',
+    demo: 'https://santimanso.com/',
+  },
+]
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
-      <h2>Porfolio</h2>
+      <h2>Portfolio</h2>
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio 1' />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a
-              href='https://github.com'
-              className='btn'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-            <a
-              href='https://github.com'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a
+                  href={github}
+                  className='btn'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className='btn btn-primary'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  live Demo
+                </a>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
